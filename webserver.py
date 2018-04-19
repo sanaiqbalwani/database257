d6782f89f0aec3fca6f3506280351953a5d82513
Sana Iqbal <sanaiqbalsana@yahoo.in>
2018-04-19 13:56:28 -0700

    added files with ui

--- webserver.py
+++ webserver.py
@@ -1,15 +1,17 @@
 import sqlite3
 
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+from flask import Flask, render_template, request, redirect,flash,url_for
 
+
+app = Flask(__name__)
+app.secret_key = 'this_is_secret'
 
 
 from dotmap import DotMap
 from bs4 import BeautifulSoup
 
 
-@app.route("/create_tables", methods=["GET"])
+@app.route("/", methods=["GET"])
 def create_tables():
 	'''this funtion creates tables in our database if they do not exist'''
 
@@ -34,7 +36,8 @@ def create_tables():
 	c.execute("""
 			CREATE TABLE IF NOT EXISTS owner
         	(	host_id integer PRIMARY KEY AUTOINCREMENT,
-	        	hostname text NOT NULL, 
+	        	firstname text NOT NULL, 
+	        	lastname text NOT NULL,
 	        	phone varchar(10) NOT NULL,
 	        	email_id text NOT NULL
 	        );""")
@@ -55,25 +58,24 @@ def create_tables():
 	connection.commit()
 	connection.close()
 
-	return "OK"
+	return render_template("home.html")
+
+	# return "OK"
 
 
-@app.route("/sign_up_form_owner", methods=["GET"])
-def sign_up_form_owner():
-	return render_template("sign_up_form_owner.html")
+@app.route("/sign_up", methods=["GET"])
+def sign_up_render():
+	return render_template("sign_up.html")
 
-@app.route("/sign_up_form_guest", methods=["GET"])
-def sign_up_form_guest():
-	return render_template("sign_up_form_guest.html")
 
-@app.route("/sign_up_form_owner", methods=["POST"])
-def sign_up_form_owner_post():
+@app.route("/sign_up", methods=["POST"])
+def sign_up():
 
 
 
 # 1. get all id in class form-group from the html instead of writing all of them
 # 2. get values of fields from form
-	html_soup = BeautifulSoup(open("templates/sign_up_form_owner.html"), 'html.parser')
+	html_soup = BeautifulSoup(open("templates/sign_up.html"), 'html.parser')
 	body=html_soup.find('body')
 	formsoup=body.find('form')
 	form_group=formsoup.find('div',class_='form-group')
@@ -82,53 +84,42 @@ def sign_up_form_owner_post():
 	    name = input_entry.get('id')
 	    d[name]=request.form[name]
 
+
 # allow dot notation in dict above 
 	d = DotMap(d)
+
 	    # d.append(name)
 	    # d[name]=name
-	print(d)
-
-	# email = request.form["email"]
-	# password = request.form["password"]
-	# location = request.form["location"]
-	# essay = request.form["essay"]
+	# print(d)
 
 # 3. Populate tables
 	connection = sqlite3.connect('user.db')
 	c = connection.cursor()
-
-	c.execute(""" INSERT INTO location (latitude,longitude,country,state,city,street,zip_code)
-		VALUES (?, ?, ?, ?,?,?,?)""", [d.latitude,d.longitude,d.country,d.state,d.city,d.street,d.zip_code])
-	c.execute(""" INSERT INTO owner (hostname, phone,email_id)
-		VALUES (?, ?, ?, ?)""", [d.hostname, d.phone,d.email_id])
+	c.execute(""" INSERT INTO owner (firstname,lastname, phone,email_id)
+		VALUES (?, ?,? ,?)""", [d.firstname, d.lastname,d.phone,d.email_id])
+	# GET host_id based on hostname from owner table 
+	host_id=c.execute("""SELECT host_id FROM owner WHERE firstname=? AND lastname=?""",(d.firstname,d.lastname))
+	host_id=host_id.fetchone()[0]
+	c.execute(""" INSERT INTO location (latitude,longitude,country,state,city,street,zip_code,host_id)
+		VALUES (?, ?, ?, ?,?,?,?,?)""", [d.latitude,d.longitude,d.country,d.state,d.city,d.street,d.zip_code,host_id])
+	
 	connection.commit()
 
-	return render_template("sign_up_form_owner.html", **d)
-
-@app.route("/show_all_users", methods=["GET"])
-def show_all_users():
-	 
-	connection = sqlite3.connect('user.db')
-	c = connection.cursor()
-
-	data = []
-	for db_row in c.execute(""" SELECT email, location, essay
-			FROM user """):
-		data_row = {}
-		data_row["email"] = db_row[0]
-		data_row["location"] = db_row[1]
-		data_row["essay"] = db_row[2]
-		data.append(data_row)
-
-	return render_template("show_all_users.html", data=data)
-
-@app.route("/", methods=["GET"])
-def login_form():
-	return render_template("login_form.html")
+	message=('Welcome! You have successfully created your OWNER account! ')
+	# return redirect(url_for('login_form'),message=message)
+	return render_template("sign_up.html",message=message) #**d)
 
 
-@app.route("/login", methods=["POST"])
-def login():
+
+@app.route("/log_in", methods=["GET"])
+def log_in_render():
+	message=None
+	return render_template("log_in.html",message=message)
+
+
+@app.route("/log_in", methods=["POST"])
+def log_in():
+
 	email = request.form["email"]
 	password = request.form["password"]
 
@@ -150,6 +141,23 @@ def login():
 	else:
 		# if user and password is not correct, go here
 		return redirect("/")
+
+@app.route("/show_all_users", methods=["GET"])
+def show_all_users():
+	 
+	connection = sqlite3.connect('user.db')
+	c = connection.cursor()
+
+	data = []
+	for db_row in c.execute(""" SELECT email, location, essay
+			FROM user """):
+		data_row = {}
+		data_row["email"] = db_row[0]
+		data_row["location"] = db_row[1]
+		data_row["essay"] = db_row[2]
+		data.append(data_row)
+
+	return render_template("show_all_users.html", data=data)
 
 if __name__ == "__main__":
     app.run()
