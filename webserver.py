09058957508e3e44d6a2b8a0ad440a18ba2b36ad
Sana Iqbal <sanaiqbalsana@yahoo.in>
2018-04-20 02:26:29 -0700

    added new all required html pages in layout

--- webserver.py
+++ webserver.py
@@ -62,6 +62,9 @@ def create_tables():
 
 	# return "OK"
 
+@app.route("/home", methods=["GET"])
+def render_home():
+	return render_template("home.html")
 
 @app.route("/sign_up", methods=["GET"])
 def sign_up_render():
@@ -87,28 +90,61 @@ def sign_up():
 
 # allow dot notation in dict above 
 	d = DotMap(d)
+# 3. Populate tables
+	connection = sqlite3.connect('user.db')
+	c = connection.cursor()
+	if d.user_type=='owner':
+		c.execute(""" INSERT INTO owner (firstname,lastname, phone,email_id)
+			VALUES (?, ?,? ,?)""", [d.firstname, d.lastname,d.phone,d.email_id])
+		# GET host_id based on hostname from owner table 
+		
+		connection.commit()
+	elif d.user_type=="guest": 
+		c.execute(""" INSERT INTO guest (firstname,lastname, phone,email_id)
+		VALUES (?, ?,? ,?)""", [d.firstname, d.lastname,d.phone,d.email_id])
+		# G
 
-	    # d.append(name)
-	    # d[name]=name
-	# print(d)
+	message=('Welcome! You have successfully created your account! ')
+	# return redirect(url_for('login_form'),message=message)
+	return render_template("sign_up.html",message=message) #**d)
+
+@app.route("/list_property", methods=["GET"])
+def list_property_render():
+	return render_template("list_property.html")
+
+
+@app.route("/list_property", methods=["POST"])
+def list_property():
+# 1. get all id in class form-group from the html instead of writing all of them
+# 2. get values of fields from form
+	html_soup = BeautifulSoup(open("templates/list_property.html"), 'html.parser')
+	body=html_soup.find('body')
+	formsoup=body.find('form')
+	form_group=formsoup.find('div',class_='form-group')
+	d={}
+	for input_entry in form_group.find_all('input',class_='form-control'):
+	    name = input_entry.get('id')
+	    d[name]=request.form[name]
+
+
+# allow dot notation in dict above 
+	d = DotMap(d)
 
 # 3. Populate tables
 	connection = sqlite3.connect('user.db')
 	c = connection.cursor()
-	c.execute(""" INSERT INTO owner (firstname,lastname, phone,email_id)
-		VALUES (?, ?,? ,?)""", [d.firstname, d.lastname,d.phone,d.email_id])
 	# GET host_id based on hostname from owner table 
-	host_id=c.execute("""SELECT host_id FROM owner WHERE firstname=? AND lastname=?""",(d.firstname,d.lastname))
-	host_id=host_id.fetchone()[0]
+	host_id=int(1111111) #filler
+	# host_id=c.execute("""SELECT host_id FROM owner WHERE firstname=? AND lastname=?""",(d.firstname,d.lastname))
+	# host_id=host_id.fetchone()[0]
 	c.execute(""" INSERT INTO location (latitude,longitude,country,state,city,street,zip_code,host_id)
 		VALUES (?, ?, ?, ?,?,?,?,?)""", [d.latitude,d.longitude,d.country,d.state,d.city,d.street,d.zip_code,host_id])
 	
 	connection.commit()
 
-	message=('Welcome! You have successfully created your OWNER account! ')
+	message=('You have successfully listed a property! Get ready to handle money and guests :) ')
 	# return redirect(url_for('login_form'),message=message)
-	return render_template("sign_up.html",message=message) #**d)
-
+	return render_template("list_property.html",message=message) #**d)
 
 
 @app.route("/log_in", methods=["GET"])
@@ -122,28 +158,55 @@ def log_in():
 
 	email = request.form["email"]
 	password = request.form["password"]
+	user_type=request.form["user_type"]
 
 	connection = sqlite3.connect('user.db')
 	c = connection.cursor()
 
-	c.execute("""SELECT count(*) 
-		FROM user
-		WHERE email = ?
-		AND password = ?""", [email, password])
+	if user_type=='owner':
+
+		c.execute("""SELECT count(*) 
+			FROM owner
+			WHERE email = ?
+			AND password = ?""", [email, password])
+
+	elif user_type=='guest':
+
+		c.execute("""SELECT count(*) 
+			FROM guest
+			WHERE email = ?
+			AND password = ?""", [email, password])
+
 
 	# Results into (0), or (1)
 
 	count = c.fetchone()[0]
+	#####
+		#add code for serach here
+	######
 
 	if count > 0:
 		# if user and password is correct, go here
 		return redirect("/show_all_users")
 	else:
 		# if user and password is not correct, go here
-		return redirect("/")
+		return redirect("results.html")
 
-@app.route("/show_all_users", methods=["GET"])
-def show_all_users():
+
+
+@app.route("/search_property", methods=["GET"])
+def search_property():
+
+	return render_template("search_property.html")
+
+
+@app.route("/results", methods=["GET","POST"])
+def results():
+
+
+	#####
+		#add code for serach here
+	######
 	 
 	connection = sqlite3.connect('user.db')
 	c = connection.cursor()
@@ -157,7 +220,7 @@ def show_all_users():
 		data_row["essay"] = db_row[2]
 		data.append(data_row)
 
-	return render_template("show_all_users.html", data=data)
+	return render_template("results.html", data=data)
 
 if __name__ == "__main__":
     app.run()
